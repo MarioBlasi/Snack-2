7f4efa7448de45967a1af3fdeb041aff88a3f59d
MarioBlasi <mblasi509@gmail.com>
2023-03-23 11:32:40 +0000

    array

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,10 @@ le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
 
 Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’]*/
 
-const nomiStringhe = [
-  {
-    nome: "Pippo",
-    nome: "Pluto",
-    nome: "Paperino",
-  },
-];
+const nomiStringhe = ["Pippo", "Pluto", "Paperino"];
+
+console.log(nomiStringhe);
+
+const secondonomiStringhe = [];
+
+console.log(secondonomiStringhe);
